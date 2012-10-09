664f209036f36db84ef97446703a032959e71046
Palantir Seven <palantir7@outlook.com>
2012-10-09 14:07:34 +0200

    update algo

--- src/algo/InsertionSorter.java
+++ src/algo/InsertionSorter.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package algo;
 
@@ -7,75 +7,72 @@ import io.Console;
 
 /**
  * Insertion Sorter
- * 
+ *
  * @author Marcel Buchmann (s778451)
  * @email marcel.buchmann(- at -)googlemail.com
  * @version 1.0.0
  * @date 06.10.2012
  * @project de.bht.alg.s778451.sorter
- * 
+ *
  * @Command InsertionSorter s = new InsertionSorter(); s.sort(array);
  */
 public class InsertionSorter {
 
-	/**
-	 * private Area of Values
-	 */
-	private int[] unsorted;
+    /**
+     * private Area of Values
+     */
+    private int[] unsorted;
+    /**
+     * public static Area
+     */
+    public static double time;
 
-	/**
-	 * public static Area
-	 */
-	public static double time;
+    /**
+     * entry point of sorter
+     *
+     * @param array a array of integer values
+     */
+    public int[] sort(int[] array) {
 
-	/**
-	 * entry point of sorter
-	 * 
-	 * @param array
-	 *            a array of integer values
-	 */
-	public int[] sort(int[] array) {
+        this.unsorted = array;
+        insertionSort(array);
 
-		this.unsorted = array;
-		insertionSort(array);
+        return this.unsorted;
+    }
 
-		return this.unsorted;
-	}
+    /**
+     * core of insertion algorithm sorting
+     *
+     * @param array a array of integers
+     */
+    private void insertionSort(int[] array) {
 
-	/**
-	 * core of insertion algorithm sorting
-	 * 
-	 * @param array
-	 *            a array of integers
-	 */
-	private void insertionSort(int[] array) {
+        // local variables (incoming and outgoing)
+        int i;
+        int o;
 
-		// local variables (incoming and outgoing)
-		int i;
-		int o;
+        final long timeStart = System.nanoTime();
 
-		final long timeStart = System.nanoTime();
+        // sorting algorithm
+        for (o = 1; o < array.length; o++) {
+            // buffer
+            int temp = array[o];
 
-		// sorting algorithm
-		for (o = 1; o < array.length; o++) {
-			// buffer
-			int temp = array[o];
+            i = o;
 
-			i = o;
+            while (i > 0 && array[i - 1] >= temp) {
+                array[i] = array[i - 1];
+                --i;
+            }
+            Console.output(array);
 
-			while (i > 0 && array[i - 1] >= temp) {
-				array[i] = array[i - 1];
-				--i;
-			}
-			Console.output(array);
+            array[i] = temp;
+        }
 
-			array[i] = temp;
-		}
+        final long timeEnd = System.nanoTime();
 
-		final long timeEnd = System.nanoTime();
-		
-		InsertionSorter.time = ((Math.round((timeEnd - timeStart) / 1000)) / 1000);
+        InsertionSorter.time = ((Math.round((timeEnd - timeStart) / 1000)) / 1000);
 
-		Console.textBuffer = Console.textBuffer + "\n\n Laufzeit: " + InsertionSorter.time + "\n";
-	}
+        Console.textBuffer = Console.textBuffer + "\n\n Laufzeit: " + InsertionSorter.time + "\n";
+    }
 }
--- src/algo/SelectionSorter.java
+++ src/algo/SelectionSorter.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package algo;
 
@@ -7,82 +7,78 @@ import io.Console;
 
 /**
  * Selection Sorter
- * 
+ *
  * @author Marcel Buchmann (s778451)
  * @email marcel.buchmann(- at -)googlemail.com
  * @version 1.0.0
  * @date 06.10.2012
  * @project de.bht.alg.s778451.sorter
- * 
+ *
  * @Command SelectionSorter s = new SelectionSorter(); s.sort(array);
  */
 public class SelectionSorter {
 
-	/**
-	 * private Area of Values
-	 */
-	private int[] unsorted;
+    /**
+     * private Area of Values
+     */
+    private int[] unsorted;
+    /**
+     * public static Area
+     */
+    public static double time;
 
-	/**
-	 * public static Area
-	 */
-	public static double time;
+    /**
+     * entry point of sorter
+     *
+     * @param array a array of integer values
+     */
+    public int[] sort(int[] array) {
 
-	/**
-	 * entry point of sorter
-	 * 
-	 * @param array
-	 *            a array of integer values
-	 */
-	public int[] sort(int[] array) {
+        final long timeStart = System.nanoTime();
 
-		final long timeStart = System.nanoTime();
+        this.unsorted = array;
+        selectionSort(array);
 
-		this.unsorted = array;
-		selectionSort(array);
+        final long timeEnd = System.nanoTime();
+        SelectionSorter.time = ((Math.round((timeEnd - timeStart) / 1000)) / 1000);
 
-		final long timeEnd = System.nanoTime();		
-		SelectionSorter.time = ((Math.round((timeEnd - timeStart) / 1000)) / 1000);
+        Console.textBuffer = Console.textBuffer + "\n\n Laufzeit: " + SelectionSorter.time + "\n";
 
-		Console.textBuffer = Console.textBuffer + "\n\n Laufzeit: " + SelectionSorter.time + "\n";
+        return this.unsorted;
+    }
 
-		return this.unsorted;
-	}
+    /**
+     * core of selection algorithm sorting
+     *
+     * @param array a array of integers
+     */
+    private void selectionSort(int[] array) {
 
-	/**
-	 * core of selection algorithm sorting
-	 * 
-	 * @param array
-	 *            a array of integers
-	 */
-	private void selectionSort(int[] array) {
-		
-		// i = input
-		// o = output
-		// m = minimum
-		for (int o = 0; o < array.length - 1; o++) {
-			int m = o;
-			for (int i = o + 1; i < array.length; i++) {
-				if (array[i] < array[m]) m = i;
-			}
-			replace(array, o, m);
-		}
-	}
+        // i = input
+        // o = output
+        // m = minimum
+        for (int o = 0; o < array.length - 1; o++) {
+            int m = o;
+            for (int i = o + 1; i < array.length; i++) {
+                if (array[i] < array[m]) {
+                    m = i;
+                }
+            }
+            replace(array, o, m);
+        }
+    }
 
-	/**
-	 * replacer
-	 * 
-	 * @param array
-	 *            a array of integers
-	 * @param one
-	 *            integer one
-	 * @param two
-	 *            integer two
-	 */
-	private void replace(int[] array, int one, int two) {
-		int temp = array[one];
-		array[one] = array[two];
-		array[two] = temp;
-		Console.output(array);
-	}
+    /**
+     * replacer
+     *
+     * @param array a array of integers
+     * @param one integer one
+     * @param two integer two
+     */
+    private void replace(int[] array, int one, int two) {
+        int temp = array[one];
+        array[one] = array[two];
+        array[two] = temp;
+        Console.output(array);
+    }
 }
